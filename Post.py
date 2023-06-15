2daed93d7f370215847cd5549b43e997f751b8e8
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-06-15 11:31:56 +0300

    updated

--- Post.py
+++ Post.py
@@ -1,10 +1,11 @@
 class Post:
-    def __init__(self, id, title,body,author_id):
+    def __init__(self, id, title,body,author):
         self.id = id
         self.title = title
         self.body = body
 
-        self.author_id = author_id
+        self.author = author
+        self.comments = []
     
     def __str__(self):
-        return f"Post({self.id})\n{self.title}\n{self.body} ({self.author_id})\n"
+        return f"Post({self.id})\n{self.title}\n{self.body} ({self.author})\n"
