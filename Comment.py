42830f7d285a1372e6bc9a43599f552e86e5b88d
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-06-15 11:31:03 +0300

    Updated

--- Comment.py
+++ Comment.py
@@ -1,10 +1,10 @@
 class Comment:
-    def __init__(self,id,body,author_id, post_id):
+    def __init__(self,id,body,author, post):
         self.id    = id        
         self.body  = body
         
-        self.author_id = author_id        
-        self.post_id = post_id
+        self.author = author      
+        self.post = post
 
     def __str__(self):
-        return f"Author({self.author_id}) Post({self.post_id})\n Coment{self.id}\n {self.body}\n"
+        return f"Author({self.author}) Post({self.post})\n Coment{self.id}\n {self.body}\n"
